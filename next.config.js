290e3328c78285d3de9bc8db330b406c6955f01c
davidsodadavid <samitnesvrstanih@gmail.com>
2025-11-05 12:16:34 +0100

    remove unused lines in next.config

--- next.config.js
+++ next.config.js
@@ -24,18 +24,6 @@ const nextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-      },
       { protocol: "https",
         hostname: "api.ateljevolja.si" 
       },
