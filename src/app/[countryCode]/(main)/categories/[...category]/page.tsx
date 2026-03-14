dee3e1c292784b8bf7c235a6ce416a77efcd4697
davidsodadavid <samitnesvrstanih@gmail.com>
2026-03-14 22:06:59 +0100

    Removes Medusa store and imported Atelje Volja, aslo for Metadata

--- src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -47,12 +47,12 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   try {
     const productCategory = await getCategoryByHandle(params.category)
 
-    const title = productCategory.name + " | Medusa Store"
+    const title = productCategory.name + " | Atelje Volja"
 
     const description = productCategory.description ?? `${title} category.`
 
     return {
-      title: `${title} | Medusa Store`,
+      title: `${title} | Atelje Volja`,
       description,
       alternates: {
         canonical: `${params.category.join("/")}`,
