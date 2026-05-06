29be7b88474315e749fa763b0a79cf07a8539e25
user <user@users-MacBook-Pro.local>
2026-05-06 18:48:18 +0200

    Align left content in product to the left on less then 1024px.

--- src/modules/products/templates/product-info/ProductInfo.tsx
+++ src/modules/products/templates/product-info/ProductInfo.tsx
@@ -8,8 +8,8 @@ type ProductInfoProps = {
 const ProductInfo = ({ product }: ProductInfoProps) => {
   return (
     <div id="product-info">
-      <div className="flex flex-col gap-y-4 lg:max-w-[280px] mx-auto">
-        <ul className="list-none font-display leading-10 w-fit mx-auto [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
+      <div className="flex flex-col gap-y-4 lg:max-w-[280px] lg:mx-auto">
+        <ul className="list-none font-display leading-10 w-fit lg:mx-auto [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
           <li data-testid="product-title" className="text-[30px] uppercase font-bold">{product.title}</li>
           <li className="text-[30px]">{product.subtitle}</li>
           <li className="text-[30px]">{product.material}</li>
