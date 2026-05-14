0ccc0a790137eca75e9c5173365a4225c04ad927
user <user@users-MacBook-Pro.local>
2026-05-14 20:51:41 +0200

    Add video instead of picture

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 import { TextImageRow } from "@modules/home/components/text-image-row/TextImageRow"
+import { FullWidthVideo } from "@modules/home/components/full-width-video/FullWidthVideo"
 
 export const metadata: Metadata = {
   title: "Atelje Volja",
@@ -29,25 +30,7 @@ export default async function Home(props: {
   return (
     <>
       <main className="content-container flex flex-col gap-12 py-20">
-        <TextImageRow url="/images/1.png">
-          <ul className="list-none font-display leading-10 w-fit mx-auto [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
-            <li className="text-[30px] font-bold">ATELJE</li>
-            <li className="text-[30px] font-bold">SHOP</li>
-            <li className="text-[30px]">Trubarjeva 55</li>
-            <li className="font-text text-sm">Paragraph text</li>
-            <li className="font-text text-sm">
-              35 is max number of characters here
-            </li>
-            <li className="font-text text-sm">Lorem ipsum dolor sit</li>
-            <li className="font-text text-sm">Circular approach</li>
-            <li className="font-text text-sm">Facts only</li>
-            <li className="font-text text-sm">Menswear something</li>
-            <li className="font-text text-sm">Line 7</li>
-            <li className="font-text text-sm">Line 8</li>
-            <li className="font-text text-sm">Line 9</li>
-            <li className="font-text text-sm">Max 10 lines of text</li>
-          </ul>
-        </TextImageRow>
+        <FullWidthVideo url="https://www.w3schools.com/html/mov_bbb.mp4" />
         <TextImageRow url="/images/2.png">
           <div className="text-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
---
+++ src/modules/home/components/full-width-video/FullWidthVideo.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+interface IFullWidthVideo {
+  url: string
+}
+
+export const FullWidthVideo: FC<IFullWidthVideo> = ({ url }) => {
+  return (
+    <div className="relative w-full" style={{ height: "70vh" }}>
+      <video
+        src={url}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+    </div>
+  )
+}
