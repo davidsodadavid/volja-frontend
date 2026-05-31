cf9329d9e17c8fbd1f0f3af72eee1e2606d30a8a
user <user@users-MacBook-Pro.local>
2026-05-31 17:19:23 +0200

    Added right video.

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -30,7 +30,7 @@ export default async function Home(props: {
   return (
     <>
       <main className="content-container flex flex-col gap-12 py-20">
-        <FullWidthVideo url="https://www.w3schools.com/html/mov_bbb.mp4" />
+        <FullWidthVideo url="/video/volja.mov" />
         <TextImageRow url="/images/2.png">
           <div className="text-sm">
            Atelje Volja is a menswear studio and shop at Trubarjeva 55 in Ljubljana. This website runs on a
