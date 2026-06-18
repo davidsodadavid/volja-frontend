2c43a0c6154c3e96a6d8c6e2cb5142e895a8ab0e
user <user@users-MacBook-Pro.local>
2026-06-18 17:32:07 +0200

    Fix the position of the text.

--- src/modules/products/templates/product-info/ProductInfo.tsx
+++ src/modules/products/templates/product-info/ProductInfo.tsx
@@ -14,28 +14,53 @@ const ProductInfo = ({ product, variant }: ProductInfoProps) => {
   const description = variant?.metadata?.description || product.description
 
   return (
-    <div id="product-info" className="self-stretch flex flex-col">
-      <div className="flex flex-col gap-y-4 max-w-[280px] flex-1">
-        <ul className="list-none font-display leading-8 w-full lg:mx-auto [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
-          <li data-testid="product-title" className="text-[30px] uppercase font-bold">{title}</li>
-          {subtitle && <li className="text-[30px]">{subtitle}</li>}
-          {material && <li className="text-[30px]">{material}</li>}
-          {description
-            ?.split("\n")
-            .filter(line => line.trim())
-            .map((line, index) => (
-              <li key={index} className="font-text text-sm break-words">
-                {line}
-              </li>
-            ))}
-        </ul>
-        <p className="text-xs mt-auto lg:pt-48">
-          Every piece has its unique date of birth embroidered.<br />
-          Make yours last the longest, give it a full life, then<br />
-          return it to us for repairs, reselling or disposal.
-        </p>
-      </div>
-    </div>
+   <div
+  id="product-info"
+  className="self-stretch flex flex-col h-full"
+>
+  <div className="flex flex-col max-w-[280px] flex-1">
+    <ul className="flex-1 list-none font-display leading-8 w-full lg:mx-auto [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
+      <li
+        data-testid="product-title"
+        className="text-[30px] uppercase font-bold"
+      >
+        {title}
+      </li>
+
+      {subtitle && (
+        <li className="text-[30px]">
+          {subtitle}
+        </li>
+      )}
+
+      {material && (
+        <li className="text-[30px]">
+          {material}
+        </li>
+      )}
+
+      {description
+        ?.split("\n")
+        .filter((line) => line.trim())
+        .map((line, index) => (
+          <li
+            key={index}
+            className="font-text text-sm break-words"
+          >
+            {line}
+          </li>
+        ))}
+    </ul>
+  </div>
+
+  <p className="text-xs mt-auto pt-4 ml-4">
+    Every piece has its unique date of birth embroidered.
+    Make yours last the longest,
+    <br />
+    give it a full life, then
+    return it to us for repairs, reselling or disposal.
+  </p>
+</div>
   )
 }
 
