d010e835ee707d0f364adf4785c5da8ba1cd1b55
user <user@users-MacBook-Pro.local>
2026-06-24 10:40:09 +0200

    Fix tile for the mobile view.

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -35,25 +35,25 @@ export default async function ProductPreview({
         className={thumbnailClassName}
       />
       <div
-        className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex flex-col justify-between p-4"
+        className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 flex flex-col justify-between max-[500px]:p-2 p-4"
         style={{ backgroundColor: bgColor }}
       >
         {/*description*/}
-        <div className="flex flex-col gap-y-4 lg:max-w-[280px]">
-          <ul className="list-none font-display leading-5 lg:leading-7 xl:leading-8 w-full break-words [&>li]:flex [&>li]:gap-[10px] [&>li]:items-baseline [&>li::before]:content-['–'] [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
-            <li data-testid="product-title" className="text-[16px] lg:text-[24px] xl:text-[30px] uppercase font-bold">{product.title}</li>
-            <li className="text-[16px] lg:text-[24px] xl:text-[30px]">{product.subtitle}</li>
-            {product.material && <li className="text-[16px] lg:text-[24px] xl:text-[30px]">{product.material}</li>}
+        <div className="flex flex-col max-[500px]:gap-y-0 gap-y-4 lg:max-w-[280px]">
+          <ul className="list-none font-display max-[500px]:leading-3 leading-5 lg:leading-7 xl:leading-8 w-full break-words [&>li]:flex [&>li]:max-[500px]:gap-0 [&>li]:gap-[10px] [&>li]:items-baseline [&>li]:max-[500px]:my-0 [&>li::before]:content-['–'] [&>li::before]:max-[500px]:w-3 [&>li::before]:w-6 [&>li::before]:text-center [&>li::before]:flex-shrink-0">
+            <li data-testid="product-title" className="max-[500px]:text-[11px] text-[16px] lg:text-[24px] xl:text-[30px] uppercase font-bold">{product.title}</li>
+            <li className="max-[500px]:text-[11px] text-[16px] lg:text-[24px] xl:text-[30px]">{product.subtitle}</li>
+            {product.material && <li className="max-[500px]:text-[11px] text-[16px] lg:text-[24px] xl:text-[30px]">{product.material}</li>}
             {product.description
               ?.split("\n")
               .map((line, index) => (
-                <li key={index} className="font-text text-[10px] lg:text-xs xl:text-sm">
+                <li key={index} className="font-text max-[500px]:text-[8px] text-[10px] lg:text-xs xl:text-sm">
                   {line}
                 </li>
               ))}
           </ul>
         </div>
-        {!isArchive && <p className="text-base lg:text-2xl font-text">{cheapestPrice?.calculated_price}</p>}
+        {!isArchive && <p className="max-[500px]:text-sm text-base lg:text-2xl font-text">{cheapestPrice?.calculated_price}</p>}
       </div>
     </div>
   )
