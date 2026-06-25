9415df9e2d4acaccbf6a58ae5d12f350bf04067d
user <user@users-MacBook-Pro.local>
2026-06-25 10:25:24 +0200

    Add meta-pixel to webpage.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { getBaseURL } from "@lib/util/env"
 import localFont from "next/font/local"
 import { Metadata } from "next"
+import Script from "next/script"
 import "styles/globals.css"
 import CookieConsent from "@components/cookie-consent"
 import { CookieConsentProvider } from "@components/cookie-consent/context"
@@ -48,6 +49,16 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           <main className="relative">{props.children}</main>
           <CookieConsent />
         </CookieConsentProvider>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1448491460418629');fbq('track','PageView')`,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1448491460418629&ev=PageView&noscript=1" />
+        </noscript>
       </body>
     </html>
   )
